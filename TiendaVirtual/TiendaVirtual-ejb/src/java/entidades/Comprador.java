703c222d927975fffa03b9be668edda37048d04b
Estudiante <Estudiante@Equipo24>
2017-03-08 19:51:41 +0000

    Version inicial del programa

--- TiendaVirtual/TiendaVirtual-ejb/src/java/entidades/Comprador.java
+++ TiendaVirtual/TiendaVirtual-ejb/src/java/entidades/Comprador.java
@@ -6,6 +6,8 @@
 package entidades;
 
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
@@ -14,10 +16,12 @@ import javax.persistence.OneToMany;
  * @author Estudiante
  */
 @Entity
+@DiscriminatorValue(value = "C")
 public class Comprador extends Persona{
     
     // en relaciones bidimeccionales, debemos definir el dueño de la relacion y esta va a ser el de lado muchos definido de la siguiente manera
     @OneToMany(mappedBy = "comprador") // siendo "comprador" de la Clase Orden
     private List<Orden> ordenes;
+    @Column(name = "CANTIDAD_COMPRAS")
     private int cantidadCompras;
 }
